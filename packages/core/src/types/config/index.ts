ed6f0b451a08a69b619d5534fa2bfd227452b8af
chelkyl <14041823+chelkyl@users.noreply.github.com>
2022-03-13 03:19:05 -0500

    feat: support prisma SHADOW_DATABASE_URL
    Co-authored-by: John Armstrong <jlarmstrongiv@users.noreply.github.com>

--- packages/core/src/types/config/index.ts
+++ packages/core/src/types/config/index.ts
@@ -54,6 +54,7 @@ export type { ListSchemaConfig, ListConfig, BaseFields, MaybeSessionFunction, Ma
 
 export type DatabaseConfig<TypeInfo extends BaseKeystoneTypeInfo> = {
   url: string;
+  shadowUrl?: string;
   onConnect?: (args: KeystoneContext<TypeInfo>) => Promise<void>;
   useMigrations?: boolean;
   enableLogging?: boolean;
